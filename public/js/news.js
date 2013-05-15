889950da17e3d3ca1951dc7748ad890d1ee3261c
Luchs <Luchs@euirc.eu>
2013-05-15 22:21:32 +0200

    Query news items sorted by date
    The date input has been changed to type=date for this to work properly.
    Consequently, the formatting has to be improved...

--- public/js/news.js
+++ public/js/news.js
@@ -3,7 +3,7 @@
 angular.module('clonkspotNewsApp', [])
   .controller('NewsCtrl', function($scope) {
     // Load the news from the server.
-    dpd.news.get({$limit: 4}, function(news, error) {
+    dpd.news.get({$limit: 4, $sort: {date: -1}}, function(news, error) {
       $scope.news = news
       $scope.$apply()
     })
@@ -16,12 +16,21 @@ angular.module('clonkspotNewsApp', [])
       $scope.editItem = to
     }
 
+    // Adds another news item on top.
+    $scope.addItem = function() {
+      var n = $scope.news.slice(0, 3)
+      n.unshift({})
+      $scope.news = n
+    }
+
     // Save the edited news items on the server.
     $scope.updateNewsItems = function() {
-      $scope.news.forEach(function(item) {
+      $scope.news.forEach(function(item, index) {
         dpd.news.post(item, function(result, error) {
           if (error)
             alert('There was an error while saving: ' + error)
+          else
+            $scope.news[index] = result
         })
       })
     }
