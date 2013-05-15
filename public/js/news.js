9e782950fb896c7c542c8e22bf7a37964dd864ae
Luchs <Luchs@euirc.eu>
2013-05-15 21:43:54 +0200

    Add a form for editing news items
    There's no way to save changes on the server currently.

--- public/js/news.js
+++ public/js/news.js
@@ -7,4 +7,17 @@ angular.module('clonkspotNewsApp', [])
       $scope.news = news
       $scope.$apply()
     })
+
+    // The item that is being edited.
+    $scope.editItem = 1
+
+    // Change above item.
+    $scope.changeEditItem = function(to) {
+      $scope.editItem = to
+    }
+
+    // Save the edited news item on the server.
+    $scope.updateNewsItem = function(item) {
+      console.log('Updating item', item)
+    }
   })
